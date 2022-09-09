3e2a5a7c03c8cd06e38bcfe894bdfa86829951d4
EyuaelB <eyuaelbezabeh@gmail.com>
2022-09-09 13:27:53 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from random import random
+
+import random
+rand_num = random.randint[0,10]
+
+# usr_input_num=int(input('input the number'))
+
+
+if(int(input("Guess the number I'm Holding")) == rand_num):
+    "Great, you've enterd the right number you are awrarded 10pts"
+
+elif (int(input("Guess the number I'm Holding") is None):
+    "please enter a valid number" 
+else:
+    print("Wrong, Try A gain, -10p")
