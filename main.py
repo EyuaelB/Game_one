a5f86fbe322c6447ea9c8e51c40beff7c52cee9c
EyuaelB <eyuaelbezabeh@gmail.comlear>
2022-09-21 17:09:59 +0300

    docstrings

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import utility
 from user import User
 from game import Game
 
-
-
 def main():
 
     utility.display_clear_normal('Welcome, \n')
@@ -14,27 +12,12 @@ def main():
 
     player = User(name)
     game = Game(player)
-                
-    # usr.play_game(game)
 
-    # game.start()
     print('\n')
     time.sleep(2)
     utility.display_clear_prog_str('Hello')
     utility.display_clear('I will iterate and randomly select a number from Zero to Ten including Zero and Ten you Have to Guess')
 
-
-# def prompt(player,game,num:int)->None:
-#     while True:
-#         if(usr.pts is not None and usr.pts < 50):
-#             rand_num = random.randint(0,10)
-#             # num = input('Enter Your guess \n')
-#             game.check_usr_input_num(num,rand_num)
-
-#         else:
-#             utility.display_clear_prog_str('Congrattulations')
-#             utility.display_clear_normal('your point have reache 50')
-#             print('Conne:
     while True:
         rand_num = random.randint(0,10)
         num = int(input('Enter Your guess \n'))
--- try.py
+++ try.py
@@ -1,1 +1,74 @@
-import time
+# numlist = [4, 5, 6, 9, 1, 7, 13, 8, 2]
+# sm  = float('inf')
+# ssm  = float('inf')
+# for num in numlist:
+#     if num <= sm:
+#         sm, ssm = num, sm
+#     elif num < ssm:
+#         ssm = num
+# print(sm)
+# print(ssm
+# 
+
+
+# """Counting list elements and changin to Dictionary
+# """
+
+# # my_laptop = ['Hp', 'Hp', 'Dell', 'Acer']
+
+# # final_lap_cout= {}
+# # for lap in my_laptop:
+# #     if lap not in final_lap_cout:
+# #         final_lap_cout.update({lap : 1})
+# #     else:
+# #         final_lap_cout.update({lap:final_lap_cout[lap]+1})
+        
+# # print(final_lap_cout)
+
+
+
+# """List comprehension
+# """
+
+# list1 = [i for i in range(1, 10)]
+# print(list1)
+
+# """Dictionary comprehension
+# """
+
+# dict1 = {i:i*i for i in range(1, 5)}
+# print(dict1)
+
+# """Lambda function
+# """
+
+# even = lambda n :list(range(0,n,2))
+
+# a = print(even(10))
+
+
+
+
+# ser = (1,2,3)
+# print(ser[2])
+
+
+
+# dfv = {}
+
+# print(type(dfv))
+
+
+
+
+
+
+list1= []
+my_inputs = input("entter numbers with comma separation \n")
+
+for ch in my_inputs:
+    if ch in ['0','1','2','3','4','5','6','7','8','9']:
+        list1.append(int(ch))
+        
+        
+print(list1)
